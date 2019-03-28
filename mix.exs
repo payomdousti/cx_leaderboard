4084aeb4311307853782fb0c457a4d50fb6d6468
Payom Dousti <payomdousti@gmail.com>
2019-03-27 18:29:43 -0700

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirLeaderboard.MixProject do
     [
       description: "Fast, customizable leaderboards database.",
       app: :elixir_leaderboard,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       package: package(),
