e32a83cdbb20609eab68b67785b69f95e32789be
Payom Dousti <payomdousti@gmail.com>
2019-03-27 17:05:24 -0700

    make redis url configurable

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+# Redix
+config :redix,
+  uri: System.get_env("REDIS_LEADERBOARD"),
+  root_key: "exlb"
